063907e6b4546430b7522109b1634dd275619db6
zoeyfish <45347302+zoeyfish@users.noreply.github.com>
2022-05-12 23:57:14 +0800

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -8,145 +8,138 @@ const int RightMotorForward = 5;
 const int RightMotorBackward = 4;
 
 //sensor pins
-#define trig_pin A1 //analog input 1
-#define echo_pin A2 //analog input 2
+int trigPin = A2;
+int echoPin = A1;
+int maximumcm = 250;
+int cm;
 
-#define maximum_distance 200
-boolean goesForward = false;
-int distance = 100;
-
-NewPing sonar(trig_pin, echo_pin, maximum_distance); //sensor function
+NewPing sonar(trig_pin, echo_pin, maximumcm);
 Servo servo_motor; 
 
 void setup() {
-  pinMode(RightMotorForward, OUTPUT);
-  pinMode(LeftMotorForward, OUTPUT);
-  pinMode(LeftMotorBackward, OUTPUT);
-  pinMode(RightMotorBackward, OUTPUT);
-  
-  servo_motor.attach(9); //our servo pin
+    pinMode(RightMotorForward, OUTPUT);
+    pinMode(LeftMotorForward, OUTPUT);
+    pinMode(LeftMotorBackward, OUTPUT);
+    pinMode(RightMotorBackward, OUTPUT);
 
-  servo_motor.write(115);
-  delay(2000);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
-  distance = readPing();
-  delay(100);
+    servo_motor.attach(9);
+    servo_motor.write(115);
+    delay(2000);
+    cm = readPing();
+    delay(100);
 }
 
 void loop(){
-  int distanceRight = 0;
-  int distanceLeft = 0;
-  delay(50);
+    cm = readPing();
+    int cmRight = 0;
+    int cmLeft = 0;
+    delay(50);
 
-  if (distance <= 20){
-    moveStop();
-    delay(300);
-    moveBackward();
-    delay(400);
-    moveStop();
-    delay(300);
-    distanceRight = lookRight();
-    delay(300);
-    distanceLeft = lookLeft();
-    delay(300);
+    if (distance <= 25) {
+        moveStop();
+        delay(200);
+        moveBackward();
+        delay(300);
+        moveStop();
+        delay(200);
+        cmRight = lookRight();
+        delay(200);
+        cmLeft = lookLeft();
+        delay(200);
 
-    if (distance >= distanceLeft){
-      turnRight();
-      moveStop();
+        if (cmLeft <= 25 && cmRight <= 25) {
+            moveStop();
+            delay(200);
+            moveBackward();
+            delay(300);
+            moveStop();
+            delay(200);
+            cmRight = lookRight();
+            delay(200);
+            cmLeft = lookLeft();
+            delay(200);
+            moveStop();
+            delay(200);
+        }
+    
+        else if (cmLeft > cmRight) {
+            turnLeft();
+            delay(300);
+            moveStop();
+        }
+
+        else if (cmRight > cmLeft) {
+            turnRight();
+            delay(300);
+            Stop();
+        }
     }
-    else{
-      turnLeft();
-      moveStop();
+  
+    else {
+        Forward();
     }
-  }
-  else{
-    moveForward(); 
-  }
-    distance = readPing();
+  
+    cm = readPing();
 }
 
-int lookRight(){  
-  servo_motor.write(50);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distance;
+int lookRight() {  
+    servo_motor.write(50);
+    delay(200);
+    int cm = readPing();
+    delay(200);
+    servo_motor.write(115);
+    return cm;
 }
 
 int lookLeft() {
-  servo_motor.write(170);
-  delay(500);
-  int distance = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distance;
-  delay(100);
+    servo_motor.write(170);
+    delay(200);
+    int cm = readPing();
+    delay(200);
+    servo_motor.write(115);
+    return cm;
 }
 
 int readPing() {
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm==0) {
-    cm=250;
-  }
-  return cm;
+    delay(200);
+    int cm = sonar.ping_cm();
+    if (cm==0) {
+        cm=250;
+    }
+    return cm;
 }
 
 void moveStop() {
-  digitalWrite(RightMotorForward, LOW);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-  digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorForward, LOW);
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+    digitalWrite(LeftMotorBackward, LOW);
 }
 
-void moveForward(){
-  if(!goesForward){
-    goesForward=true;
+void moveForward() {
     digitalWrite(LeftMotorForward, HIGH);
     digitalWrite(RightMotorForward, HIGH);
     digitalWrite(LeftMotorBackward, LOW);
     digitalWrite(RightMotorBackward, LOW); 
-  }
 }
 
 void moveBackward() {
-  goesForward=false;
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorForward, LOW); 
+    digitalWrite(LeftMotorBackward, HIGH);
+    digitalWrite(RightMotorBackward, HIGH);
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorForward, LOW); 
 }
 
-void turnRight(){
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorBackward, HIGH);
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorForward, LOW);
-  
-  delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+void turnRight() {
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorBackward, HIGH);
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorForward, LOW);
 }
 
 void turnLeft() {
-  digitalWrite(LeftMotorBackward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  digitalWrite(LeftMotorForward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
-
-  delay(500);
-  
-  digitalWrite(LeftMotorForward, HIGH);
-  digitalWrite(RightMotorForward, HIGH);
-  digitalWrite(LeftMotorBackward, LOW);
-  digitalWrite(RightMotorBackward, LOW);
+    digitalWrite(LeftMotorBackward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+    digitalWrite(LeftMotorForward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
 }
